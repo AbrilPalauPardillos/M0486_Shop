08c234c7ef3d83e1edb9924ccf47b435c4578fd0
Abril Palau Pardillos <abrilpalau14@gmail.com>
2026-01-23 16:53:54 +0100

    modifcación de algunos errores con la bbdd

--- src/dao/DaoImplHibernate.java
+++ src/dao/DaoImplHibernate.java
@@ -1,5 +1,7 @@
 package dao;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -7,15 +9,13 @@ import org.hibernate.cfg.Configuration;
 import model.Product;
 import model.ProductHistory;
 import model.Employee;
-import java.util.List;
-import java.util.ArrayList;
 
 public class DaoImplHibernate implements Dao { 
     private SessionFactory sessionFactory;
     private Session session;
 
     @Override
-    public void connect() {
+    public void connect() { 
         if (sessionFactory == null) {
             sessionFactory = new Configuration().configure().buildSessionFactory();
         }
@@ -23,63 +23,83 @@ public class DaoImplHibernate implements Dao {
     }
 
     @Override
-    public void disconnect() {
+    public void disconnect() { 
         if (session != null && session.isOpen()) {
             session.close();
         }
     }
 
     @Override
-    public List<Product> getInventory() {
+    public List<Product> getInventory() { 
         return session.createQuery("FROM Product", Product.class).list();
     }
 
     @Override
     public void addProduct(Product product) { 
         Transaction tx = session.beginTransaction();
-        session.save(product);
-        tx.commit();
-    }
-
-    @Override
-    public void updateProduct(Product product) {
-        Transaction tx = session.beginTransaction();
-        session.update(product);
-        tx.commit();
-    }
-
-    @Override
-    public void deleteProduct(int productId) { 
-        Transaction tx = session.beginTransaction();
-        Product p = session.get(Product.class, productId);
-        if (p != null) session.delete(p);
-        tx.commit();
-    }
-
-    @Override
-    public boolean writeInventory(List<Product> inventory) {
-        Transaction tx = null;
         try {
-            tx = session.beginTransaction();
+            session.save(product); 
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) tx.rollback();
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void updateProduct(Product product) { 
+        Transaction tx = session.beginTransaction();
+        try {
+            session.update(product);
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) tx.rollback();
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void deleteProduct(int productId) {
+        Transaction tx = session.beginTransaction();
+        try {
+            Product p = session.get(Product.class, productId);
+            if (p != null) {
+                session.delete(p); 
+            }
+            tx.commit();
+        } catch (Exception e) {
+            if (tx != null) tx.rollback();
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public boolean writeInventory(List<Product> inventory) { 
+        Transaction tx = session.beginTransaction();
+        try {
             for (Product p : inventory) {
                 ProductHistory history = new ProductHistory(p);
-                session.save(history);
+                session.save(history); 
             }
             tx.commit();
             return true;
         } catch (Exception e) {
-            if (tx != null) tx.rollback();
+            if (tx != null) tx.rollback(); 
             e.printStackTrace();
             return false;
         }
     }
 
     @Override
-    public Employee getEmployee(int employeeId, String password) {
-        return session.createQuery("FROM Employee WHERE employeeId = :id AND password = :pass", Employee.class)
-                      .setParameter("id", employeeId)
-                      .setParameter("pass", password)
-                      .uniqueResult();
+    public Employee getEmployee(int employeeId, String password) { 
+        try {
+            return session.createQuery("FROM Employee WHERE employeeId = :id AND password = :pass", Employee.class)
+                    .setParameter("id", employeeId)
+                    .setParameter("pass", password)
+                    .uniqueResult();
+        } catch (Exception e) {
+            return null;
+        }
     }
 
     @Override public Employee getEmployee(int employeeId) { return null; }
